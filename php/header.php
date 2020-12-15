0a8c2dea65641588b5c673379f51952a8f1c43dd
unknown <phigu940@student.liu.se>
2020-12-15 19:47:31 +0100

    more, index started

---
+++ php/header.php
@@ -0,0 +1,28 @@
+<?php 
+    $name = "ÖLDUNDERKUNGcupen"; 
+    $date = "29 maj 2021 09:00 – 15:00";
+    $adress = "Östra Löa 122, 714 94 Kopparberg, Sverige";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $name ?></title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/header.css">
+</head>
+<header>
+    <div class="master"><a class="master" href="index.php"><?php echo $name ?></a></div>
+    <div class="menu">
+        <a href="index.php">Hem</a>
+        <a href="sign-up.php">Anmälan</a>
+        <a href="contact.php">Kontakt</a>
+        <a href="informaton.php">Information</a>
+        <a href="gallery.php">Galleri</a>
+        <a href="https://www.instagram.com/loaolcup/">|insta|</a>
+        <a href="https://www.facebook.com/loaolcup">|face|</a>
+    </div>
+</header>
+
+</html>
--- php/index.php
+++ php/index.php
@@ -1,4 +1,16 @@
-<?php require "inc/base.php"; ?>
-<body>
-    <h1>Goddag</h1>
-</body>
+<?php require "header.php"; ?>
+
+<html>
+    <body>
+        <div class="grid-split">
+            <div><img src="img/ol.gif" alt="" ></div>
+            <div class="dynamic-height">
+                <h1><?php echo $name ?></h1>
+                <h2>
+                    <?php echo $date ?> <br><br>
+                    <?php echo $adress ?> <br>
+                </h2>
+            </div>
+        </div>
+    </body>
+</html>
