f7493a911ec3a62869a23bcbf782f7c6b1310ff3
unknown <phigu940@student.liu.se>
2020-12-15 17:42:58 +0100

    php started

---
+++ php/inc/menu.php
@@ -0,0 +1,9 @@
+<html>
+<a href="index.php">Hem</a>
+<a href="sign-up.php">Anmälan</a>
+<a href="contact.php">Kontakt</a>
+<a href="informaton.php">Information</a>
+<a href="gallery.php">Galleri</a>
+<a href="https://www.instagram.com/loaolcup/">|insta|</a>
+<a href="https://www.facebook.com/loaolcup">|face|</a>
+</html>
---
+++ php/index.php
@@ -0,0 +1,4 @@
+<?php require "inc/base.php"; ?>
+<body>
+    <h1>Goddag</h1>
+</body>
