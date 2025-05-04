35535d79bbefb82995f97925c8029555bf245767
mac Philip <pjg1890@gmail.com>
2025-05-04 15:07:11 +0200

    2025 lower expectations v1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
-const countdownDate = new Date("Mar 01, 2025 18:00:00").getTime()
-var intervalId
 
-document.addEventListener("DOMContentLoaded", function () {
-    intervalId = setInterval(countdown, 1000)
-    countdown()
-})
+function openPage(pageName, element){
+    document.getElementById("display").innerHTML = document.getElementById(pageName).text;
+    sessionStorage.setItem('currentPage', pageName);
 
-function countdown() {
-    const now = new Date().getTime()
-    if (now > countdownDate) {
-        document.getElementById("display").innerHTML = document.getElementById("anmäl").text
-        clearInterval(intervalId)
-    } else {
-        document.getElementById("display").innerHTML = document.getElementById("waiting").text
-        const timeLeft = countdownDate - now
+    // So that the navbar "hides" after click in mobileview
+    displayNav();
+}
 
-        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
-        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
-        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
-        var strDay = ' dagar, '
-        var strHour = ' timmar, '
-        var strMin = ' minuter och '
-        var strSec = ' sekunder. '
-
-        if (days === 1) strDay = ' dag, '
-        if (hours === 1) strHour = ' timme, '
-        if (minutes === 1) strMin = ' minut och '
-        if (seconds === 1) strSec = ' sekund. '
-
-        document.getElementById("days").innerHTML = days + strDay
-        document.getElementById("hours").innerHTML = hours + strHour
-        document.getElementById("mins").innerHTML = minutes + strMin
-        document.getElementById("secs").innerHTML = seconds + strSec
+function displayNav() {
+    var deviceWidth = window.outerWidth;
+    if (deviceWidth < 769){
+        document.getElementById("mobile-button-space").innerHTML = "<button id='mobile-button' onclick='toggleMobileNav()'>__<br>__<br>__<br></button>";
+        document.getElementById("nav").innerHTML = "";
+    }else {
+        document.getElementById("mobile-button-space").innerHTML = "";
+        document.getElementById("nav").innerHTML = sessionStorage.getItem('navButtons');
     }
+}
+
+function toggleMobileNav() {
+    var innerHTML = document.getElementById("nav").innerHTML;
+    if (innerHTML == ""){document.getElementById("nav").innerHTML = sessionStorage.getItem('navButtons');}
+    else {document.getElementById("nav").innerHTML = "";}
+}
+
+window.onload = function () {
+    // Show nav buttons
+    sessionStorage.setItem('navButtons', document.getElementById("nav").innerHTML);
+    displayNav();
+
+    // Show content
+    if (!sessionStorage.getItem('currentPage')){
+        sessionStorage.setItem('currentPage', "home")
+    }
+    document.getElementById("display").innerHTML = document.getElementById(sessionStorage.getItem('currentPage')).text;
+}
+
+window.onresize = function () {
+    displayNav();
 }
